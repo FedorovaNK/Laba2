05b668835c4d9592a8a64a0dac055034d6cbdf54
FedorovaNK <64545483+FedorovaNK@users.noreply.github.com>
2020-05-15 01:17:51 +0300

    Add files via upload

---
+++ WebS-Laba_2/SharedComponents/TorgObjects.BusinessLogic/DomainObjects/Ports/ITorgPointRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+namespace TorgObjects.DomainObjects.Ports
+{
+    public interface IReadOnlyTorgPointRepository
+    {
+        TaskTorgPoint> GetTorgPoint(long id);
+
+        Task<IEnumerableTorgPoint>> GetAlTorgPoints();
+
+        Task<IEnumerableTorgPoint>> QuerTorgPoints(ICriteriaTorgPoint> criteria);
+
+    }
+
+    public interface TorgPointRepository
+    {
+        Task AdTorgPointTorgPointTorgpoint);
+
+        Task RemovTorgPointTorgPointTorgpoint);
+
+        Task UpdatTorgPointTorgPointTorgpoint);
+    }
+}
